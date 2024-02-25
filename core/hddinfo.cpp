674ea5070b83278012ddfe255620d192859932d5
Roker2 <dimaminko2000@gmail.com>
2024-02-25 10:22:41 +0100

    core: hddinfo: Add creation of WindowsHddParser on Win platform

--- core/hddinfo.cpp
+++ core/hddinfo.cpp
@@ -1,11 +1,19 @@
 #include "hddinfo.hpp"
 
+#ifdef __linux__
 #include "parsers/linux/hdd_parser.hpp"
+#elif defined(_MSC_VER)
+#include "parsers/windows/hdd_parser.h"
+#endif
 
 HddInfo::pointer HddInfo::makeInLinuxWay()
 {
     HddInfo::pointer info(new HddInfo());
+#ifdef __linux__
     info->parser = LinuxHddParser::pointer(new LinuxHddParser());
+#elif defined(_MSC_VER)
+    info->parser = WindowsHddParser::pointer(new WindowsHddParser());
+#endif
     info->parse();
     return info;
 }
